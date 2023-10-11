fb4419cbcee5f466fe2bcc2121683cb3be878e72
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-11 16:23:09 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor3/Program.cs
@@ -0,0 +1,23 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+int[] A = new int[N];
+int[] B = new int[N];
+
+string[] vet = Console.ReadLine().Split(' ');
+for (int i = 0; i < N; i++) {
+    A[i] = int.Parse(vet[i]);
+}
+
+vet = Console.ReadLine().Split(' ');
+for (int i = 0; i < N; i++) {
+    B[i] = int.Parse(vet[i]);
+}
+
+int[] C = new int[N];   
+for (int i = 0; i < N; i++) {
+    C[i] = A[i] + B[i];
+}
+
+for (int i = 0; i < N; i++) {
+    Console.Write(C[i] + " ");
+}
